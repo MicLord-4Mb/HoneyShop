3a803ef3c75cebefd963b6bc42254e391899a3f8
MicLord <miclord@yahoo.com>
2026-04-07 00:49:25 +0300

    navbar fix

--- front-end/src/components/section/HeaderInner.jsx
+++ front-end/src/components/section/HeaderInner.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Logo from '../common/Logo'
+import Menu from '../common/Menu'
+import Cart from '../common/Cart'
+import './HeaderInner.css'
+
 
 const HeaderInner = () => {
   return (

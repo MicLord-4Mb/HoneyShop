c4baa33f7c43969a8e3123cc8084f495b2f23b08
MicLord <miclord@yahoo.com>
2026-04-06 22:06:32 +0300

    add assets import

---
+++ front-end/src/assets/index.js
@@ -0,0 +1,77 @@
+import h1Rev6 from "./bee-12.png";
+import cartIcon from "./cart-icon.svg";
+import chatIcon from "./chat-icon.svg";
+import quoteIcon from "./quote-icon.svg";
+import chevronIcon from "./chevron-icon.svg";
+import footerPng3 from "./footer-png3.png";
+import h1Img14 from "./h1-img-14.png";
+import homeGallery1 from "./h1-img1.jpg";
+import h1Img2 from "./h1-img2.png";
+import homeGallery6 from "./h1-img6.jpg";
+import homeGallery7 from "./h1-img7.jpg";
+import homeGallery8 from "./h1-img8.jpg";
+import homeGallery9 from "./h1-img9-1.jpg";
+import homeGallery10 from "./h1-img10-1.jpg";
+import homeGallery11 from "./h1-img11-1.jpg";
+import h1Rev1i1 from "./h1-rev-img-1-1.png";
+import h1Rev1 from "./h1-rev-img-1.png";
+import h1Rev2 from "./h1-rev-img-2.png";
+import h1Rev3 from "./h1-rev-img-3.png";
+import h1Rev4 from "./h1-rev-img-4.png";
+import h1Rev5 from "./h1-rev-img-5.png";
+import h1Rev7 from "./h1-rev-img-7.png";
+import h1Rev8 from "./h1-rev-img-8.png";
+import h1Rev9 from "./h1-rev-img-9.png";
+import h1Rev10 from "./h1-rev-img-10.png";
+import h1Rev11 from "./h1-rev-img-11.png";
+import h1Rev12 from "./h1-rev-img-12.jpg";
+import homeProduct1 from "./h1product1.jpg";
+import homeProduct2 from "./h1product2.jpg";
+import homeProduct3 from "./h1product3.jpg";
+import homeClient1 from "./home-1-client-1.png";
+import homeClient2 from "./home-1-client-2.png";
+import homeClient3 from "./home-1-client-3.png";
+import homeClient4 from "./home-1-client-4.png";
+import homeTestimonial2 from "./home-1-testimonial-2.png";
+import background from "./image.png";
+import logo from "./new-logo.png";
+
+export {
+  h1Rev6,
+  cartIcon,
+    chatIcon,
+    quoteIcon,
+    chevronIcon,
+    footerPng3,
+    h1Img14,
+    homeGallery1,
+    h1Img2,
+    homeGallery6,
+    homeGallery7,
+    homeGallery8,
+    homeGallery9,
+    homeGallery10,
+    homeGallery11,
+    h1Rev1i1,
+    h1Rev1,
+    h1Rev2,
+    h1Rev3,
+    h1Rev4,
+    h1Rev5,
+    h1Rev7,
+    h1Rev8,
+    h1Rev9,
+    h1Rev10,
+    h1Rev11,
+    h1Rev12,
+    homeProduct1,
+    homeProduct2,
+    homeProduct3,
+    homeClient1,
+    homeClient2,
+    homeClient3,
+    homeClient4,
+    homeTestimonial2,
+    background,
+    logo
+};

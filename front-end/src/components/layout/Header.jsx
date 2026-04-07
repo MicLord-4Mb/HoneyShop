e64b183cb5a406eca669181a9dcac7f872a69f90
MicLord <miclord@yahoo.com>
2026-04-07 23:56:52 +0300

    part of background added

--- front-end/src/components/layout/Header.jsx
+++ front-end/src/components/layout/Header.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import StyleSheet from './Header.module.css'
 import HeaderInner from '../section/HeaderInner'
 import HeroSection from '../section/HeroSection'
+import { h1Rev1, h1Rev7, h1Rev8 } from '../../assets'
   
 
 const Header = () => {
   return (
-    <header className={StyleSheet.hWrapper}>
+    <header className={StyleSheet.wrapper}>
+      <img src={h1Rev1} className={StyleSheet.decLeaf1} alt="" />
+      <img src={h1Rev7} className={StyleSheet.decLeaf2} alt="" />
+      <img src={h1Rev8} className={StyleSheet.decLeaf3} alt="" />
+      
       <HeaderInner />
       <HeroSection />
     </header>

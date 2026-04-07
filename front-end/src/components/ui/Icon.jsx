a8c469a17728e037fe72f9c7640420bb270fa45a
MicLord <miclord@yahoo.com>
2026-04-07 20:36:41 +0300

    icon file test

---
+++ front-end/src/components/ui/Icon.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import StyleSheet from './Cart.module.css'
+
+const Icon = () => {
+  return (
+    <div className={StyleSheet.icon}>Icon</div>
+  )
+}
+
+export default Icon

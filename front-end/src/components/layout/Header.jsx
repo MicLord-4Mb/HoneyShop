5cdd7bc19a96472cedf2cba335d12bb68490955b
MicLord <miclord@yahoo.com>
2026-04-08 02:20:32 +0300

    add badge

--- front-end/src/components/layout/Header.jsx
+++ front-end/src/components/layout/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import StyleSheet from './Header.module.css'
 import HeaderInner from '../section/HeaderInner'
 import HeroSection from '../section/HeroSection'
-import { h1Rev1, h1Rev7, h1Rev8 } from '../../assets'
+import { h1Rev1, h1Rev7, h1Rev8, h1Rev9 } from '../../assets'
   
 
 const Header = () => {
@@ -11,6 +11,7 @@ const Header = () => {
       <img src={h1Rev1} className={StyleSheet.decLeaf1} alt="" />
       <img src={h1Rev7} className={StyleSheet.decLeaf2} alt="" />
       <img src={h1Rev8} className={StyleSheet.decLeaf3} alt="" />
+      <img src={h1Rev9} className={StyleSheet.badge} alt="" />
       
       <HeaderInner />
       <HeroSection />

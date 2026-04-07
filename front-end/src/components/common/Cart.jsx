f942c07e1dfcb2f4b6371a3d97642db57a1f51eb
MicLord <miclord@yahoo.com>
2026-04-07 20:33:01 +0300

    some assets update and fix

--- front-end/src/components/common/Cart.jsx
+++ front-end/src/components/common/Cart.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import StyleSheet from './Cart.module.css'
+import Button from '../ui/Links/Button';
+import Icon from '../ui/Icon';
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <div className={StyleSheet.wrapper}>
+      <Icon name="cart" />
+      <Button text="Contact" variant="yellow" />
+    </div>
   )
 }
 

5b3bd2a4d7bb96876a47e038e89c590164d4a2e8
MicLord <miclord@yahoo.com>
2026-04-08 01:55:08 +0300

    herosection changes

--- front-end/src/components/section/HeroSection.jsx
+++ front-end/src/components/section/HeroSection.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import StyleSheet from './HeroSection.module.css'
 import Button from '../ui/Links/Button';
+import { h1Rev12 } from '../../assets'
 
 const HeroSection = () => {
   return (
     <div className={StyleSheet.wrapper}>
-      <h2 className={StyleSheet.subtitle}>FRESH & SWEET AS HONEY.</h2>
-      <h1 className={StyleSheet.title}>HONEY BEE</h1>
-      <p className={StyleSheet.description}>
-        Lorem ipsum dolor sit amet, sint nostrum mea ut, vel semper vidisse eu usu temporibus disputationi voluptatibus in ei est possit salutandi abhorrea acusata, ad cum eruditi expetendis. Ad cum eruditi expetendis.
-      </p>
-      <Button text="View More" variant="orange" />
+      <div className={StyleSheet.content}>
+        <h2 className={StyleSheet.subtitle}>FRESH & SWEET AS HONEY.</h2>
+        <h1 className={StyleSheet.title}>HONEY BEE</h1>
+        <p className={StyleSheet.description}>
+          Lorem ipsum dolor sit amet, sint nostrum mea ut, vel semper vidisse eu usu temporibus disputationi voluptatibus in ei est possit salutandi abhorrea acusata, ad cum eruditi expetendis. Ad cum eruditi expetendis.
+        </p>
+        <Button text="View More" variant="orange" className={StyleSheet.button} />
+      </div>
+      <div className={StyleSheet.imageWrapper}>
+        <img src={h1Rev12} alt="Hero" className={StyleSheet.image} />
+      </div>
     </div>
   )
 }

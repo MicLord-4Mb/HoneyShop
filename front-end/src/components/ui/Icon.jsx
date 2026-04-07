a05e0ad7ce1b6cc9d8981ba079e17aa0fcc1cd14
MicLord <miclord@yahoo.com>
2026-04-07 21:09:44 +0300

    add icon code

--- front-end/src/components/ui/Icon.jsx
+++ front-end/src/components/ui/Icon.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import StyleSheet from './Icon.module.css'
+import { ReactComponent as CartIcon } from '../../assets/icons/cart-icon.svg';
+import { ReactComponent as ChatIcon } from '../../assets/icons/chat-icon.svg';
+import { ReactComponent as Chevron } from '../../assets/icons/chevron.svg';
+import { ReactComponent as Quote } from '../../assets/icons/quote-icon.svg';
 
-const Icon = () => {
+const iconTypes = {
+  cart: CartIcon,
+  chat: ChatIcon,
+  chevron: Chevron,
+  quote: Quote,
+};
+
+const Icon = ({ name, size = 24, color, className = ''}) => {
+  const SelectedIcon = iconTypes[name];
+
+  if (!SelectedIcon) {
+    console.error(`Icon not found for name: ${name}`);
+    return null;
+  }
+
+
   return (
-    <div className={StyleSheet.icon}>Icon</div>
+    <span className={`${StyleSheet.iconConteiner} ${className}`}
+        style={{ 
+            width: size,
+            height: size, 
+            color: color || 'currentColor' 
+        }}
+    >
+        <SelectedIcon width="100%" height="100%" />
+    </span>
   )
 }
 

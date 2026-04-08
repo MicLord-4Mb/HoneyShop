885fa98b7385fcd46c592b579a9620273d41d5a0
MicLord <miclord@yahoo.com>
2026-04-08 07:29:12 +0300

    some shop fix

--- front-end/src/components/section/ProductGrid.jsx
+++ front-end/src/components/section/ProductGrid.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import StyleSheet from './ProductGrid.module.css'
 import Container from '../layout/Container'
 import ProductCard from '../common/ProductCard'
-import { background1 } from '../../assets';
+import { background1, homeProduct1, homeProduct2, homeProduct3 } from '../../assets';
   
 const ProductGrid = () => {
   return (
@@ -11,9 +11,24 @@ const ProductGrid = () => {
         <h2>Our Products</h2>
         <p>Discover our amazing collection of products!</p>
         <section className={StyleSheet.shopGrid}>
-          <ProductCard title="Product 1" price="$19.99" imageUrl="https://via.placeholder.com/150" />
-          <ProductCard title="Product 2" price="$29.99" imageUrl="https://via.placeholder.com/150" />
-          <ProductCard title="Product 3" price="$39.99" imageUrl="https://via.placeholder.com/150" />
+          <ProductCard 
+            title="Product 1" 
+            price="$19.99"
+            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." 
+            imageUrl={homeProduct1} 
+          />
+          <ProductCard 
+            title="Product 2" 
+            price="$29.99"
+            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." 
+            imageUrl={homeProduct2} 
+          />
+          <ProductCard 
+            title="Product 3" 
+            price="$39.99"
+            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." 
+            imageUrl={homeProduct3} 
+          />
         </section>
       </Container>
     </div>

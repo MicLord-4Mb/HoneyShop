e309e462593690f52290040b7578a011ffdf892d
MicLord <miclord@yahoo.com>
2026-04-08 05:48:07 +0300

    testemonial starts

--- front-end/src/components/section/Testimonial.jsx
+++ front-end/src/components/section/Testimonial.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import StyleSheet from './Testimonial.module.css'
 
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
+    <div className={StyleSheet.wrapper}>
+      <div className={StyleSheet.testimonial}>
+        <p className={StyleSheet.quote}>"This is a great product!"</p>
+        <p className={StyleSheet.author}>- John Doe</p>
+      </div>
+    </div>
   )
 }
 

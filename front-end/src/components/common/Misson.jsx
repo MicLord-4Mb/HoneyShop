4606d4084c8fcae070897409d858148fa99c7e8e
MicLord <miclord@yahoo.com>
2026-04-08 18:48:04 +0300

    add design of mission section

--- front-end/src/components/common/Misson.jsx
+++ front-end/src/components/common/Misson.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import styleSheet from './Misson.module.css'
+import Container from '../layout/Container'
+import { homeGallery1, h1Img2 } from '../../assets'
 
 const Misson = () => {
   return (
-    <section>Misson</section>
+    <section className={styleSheet.wrapper}>
+      <Container className={styleSheet.contentContainer}>
+        <div className={styleSheet.imageContainer}>
+          <img src={homeGallery1} alt="Gallery 1" />
+        </div>
+
+        <div className={styleSheet.textContainer}>
+          <h2 className={styleSheet.title}>Our Mission</h2>
+          <p className={styleSheet.desc}>
+            The bees live as they naturally would and their benefits
+            reach more people. Our goal is to raise  <strong>San Diego’s bee
+            population</strong> throughout the city and at the same time <strong>spread
+            awareness among the community.</strong>
+          </p>
+          <p className={styleSheet.desc}>
+            Our hope is that these backyard hives will facilitate
+            a dialogue among neighbours, friends, family and the community
+            at large about the huge importance of bees.
+          </p>
+        </div>
+
+        <img src={h1Img2} alt="sign" className={styleSheet.signImage}/>
+
+      </Container>
+    </section>
   )
 }
 

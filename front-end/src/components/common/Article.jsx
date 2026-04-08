a7fc606ef9fce0da2ab27aab0bc153ea312021c2
MicLord <miclord@yahoo.com>
2026-04-08 12:29:59 +0300

    add article

---
+++ front-end/src/components/common/Article.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import StyleSheet from './Article.module.css'
+
+const Article = () => {
+  return (
+    <div className={StyleSheet.article}>Article</div>
+  )
+}
+
+export default Article
